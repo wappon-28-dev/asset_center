e8685441a6157faaf0f1469f4022b91cd2438de4
wappon-28-dev <wappon_28_dev@ruru.be>
2023-07-11 16:53:09 +0900

    🚧 一覧の実装
    - 単体はまた今度

--- client/src/routes/public/[...path]/+page.svelte
+++ client/src/routes/public/[...path]/+page.svelte
@@ -1,39 +1,103 @@
 <script lang="ts">
-  import { client } from "$lib/model/service/client";
+  import Inquiry from "$lib/assets/Inquiry.svelte";
+  import DownloadDialog from "$lib/components/DownloadDialog.svelte";
+  import { runTransitionRaw } from "$lib/model/constants";
   import { isLoading } from "$lib/model/store";
-  import type { InferResponseType } from "hono";
-  import { onMount } from "svelte";
+  import IconButton from "@smui/icon-button";
+  import List, { Graphic, Item, Label, Meta } from "@smui/list";
+  import FileDownloadOutline from "svelte-material-icons/FileDownloadOutline.svelte";
+  import FolderOutline from "svelte-material-icons/FolderOutline.svelte";
+  import TrayArrowDown from "svelte-material-icons/TrayArrowDown.svelte";
   import type { PageData } from "./$types";
-
   export let data: PageData;
-  const dirPath = data.pathname.split("/").slice(2).join("/");
+  let openIndex: number[] = [];
 
-  const getChildren = client.v1.assets.public.children.$get;
-  let children: InferResponseType<typeof getChildren>;
-
-  onMount(async () => {
-    const res = await getChildren({
-      query: {
-        dirPath,
-      },
-    });
-
-    if (res.ok) {
-      children = await res.json();
-    }
-    $isLoading = false;
-  });
+  const { getChildren } = data;
+  const info = getChildren().finally(() => ($isLoading = false));
 </script>
 
 <div class="container">
   <article>
-    Directoryを列挙する
-    <div class="text-container">
-      <div>{data.pathname}</div>
-      {JSON.stringify(children)}
-    </div>
+    {#await info}
+      メタデータを読み込み中です…
+    {:then { item, children }}
+      <br />
+      <Inquiry scale={1.5} /><br />
+      <List class="demo-list" dense>
+        {#each children.value as child, index}
+          <Item
+            on:click={() => runTransitionRaw(`${data.pathname}/${child.name}`)}
+          >
+            <Graphic>
+              {#if child.folder == null}
+                <FileDownloadOutline size={40} />
+              {:else}
+                <FolderOutline size={40} />
+              {/if}
+            </Graphic>
+            <Label>{child.name}</Label>
+
+            <Meta>
+              <IconButton
+                on:click={() => {
+                  if (child["@microsoft.graph.downloadUrl"] == null) return;
+                  openIndex = [];
+                  openIndex.push(index);
+                  openIndex = [...openIndex];
+                }}
+              >
+                <TrayArrowDown />
+              </IconButton>
+            </Meta>
+          </Item>
+          <DownloadDialog
+            open={openIndex.includes(index)}
+            url={child["@microsoft.graph.downloadUrl"]}
+            fileName={child.name}
+            size={child.size}
+          />
+        {/each}
+      </List>
+    {:catch err}
+      {err}
+    {/await}
   </article>
 </div>
 
 <style lang="scss">
+  :global(.mdc-circular-progress__indeterminate-circle-graphic) {
+    stroke: var(--m3-primary);
+  }
+
+  :global(.mdc-linear-progress__buffer-bar) {
+    background-color: var(--m3-inverse-primary);
+    opacity: 0.5;
+  }
+
+  .container {
+    height: calc(100vh - var(--app-bar-height));
+    display: table;
+    margin: 0 auto;
+
+    article {
+      display: table-cell;
+      vertical-align: middle;
+      text-align: center;
+
+      .info {
+        .name {
+          .title {
+            font-size: 2rem;
+            font-weight: bold;
+          }
+        }
+
+        .meta {
+          line-height: 0.8;
+          font-size: small;
+          color: var(--m3-outline);
+        }
+      }
+    }
+  }
 </style>
--- client/src/routes/public/[...path]/+page.ts
+++ client/src/routes/public/[...path]/+page.ts
@@ -1,1 +1,33 @@
+import { client } from "$lib/model/service/client";
+import type { InferResponseType } from "hono";
+import type { PageLoad } from "./$types";
+
+export const load = (async ({ params }) => {
+  const dirPath = params.path;
+  const apiGetChildren = client.v1.assets.public.children.$get;
+
+  async function getChildren(): Promise<
+    InferResponseType<typeof apiGetChildren>
+  > {
+    const res = await apiGetChildren({
+      query: {
+        dirPath,
+      },
+    });
+    const data = await res.json();
+
+    if (res.ok) {
+      return data;
+    } else {
+      if (res.status === 404) {
+        throw new Error("ASSET_NOT_FOUND");
+      } else {
+        throw new Error(`FETCH_ERROR ${String(await res.text())}`);
+      }
+    }
+  }
+
+  return { getChildren };
+}) satisfies PageLoad;
+
 export const prerender = false;
